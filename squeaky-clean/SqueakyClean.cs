4b0984516fb11c0fc46a9778ba8ead43a4a13721
mir-src <264200162+mir-src@users.noreply.github.com>
2026-07-03 11:45:05 +0300

    Tasks succeded: 7, SqueakyClean.cs

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,7 +1,42 @@
+using System.Text;
+using System.Text.RegularExpressions;
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        identifier = Regex.Replace(identifier, @"[\d-]", string.Empty);
+        identifier = Regex.Replace(identifier, @"[\d-]", string.Empty);
+
+        var actions = new Dictionary<string, Func<string, string>>
+        {
+            {@"\s", id => id.Replace(" ", "_")},
+            {"\0", id => id.Replace("\0", "CTRL")},
+            {"-", ConvertKebabToCamel}
+        };
+
+        foreach (var item in actions)
+        {
+            if (Regex.IsMatch(identifier, item.Key))
+            {
+                identifier = item.Value(identifier);
+            }
+        }
+
+        return identifier;
+    }
+    public static string ConvertKebabToCamel(string id)
+    {
+        // Split string by '-' 
+        var withoutHyphens = id.Split('-');
+        // Split returns a string[] array so put the first string in a StringBuilder
+        var withCamelCase = new StringBuilder(withoutHyphens[0]);
+        // Loop from the second string in the string[] to the end of the string[]
+        for (int i = 1; i < withoutHyphens.Length; i++)
+        {
+            withCamelCase.Append(char.ToUpper(withoutHyphens[i][0]));
+            withCamelCase.Append(withoutHyphens[i].Substring(1));
+        }
+        string result = withCamelCase.ToString();
+        return result;
     }
 }
